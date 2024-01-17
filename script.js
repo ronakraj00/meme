4f6ad5a8bad13ea6e2bd03880fc15d2dd4c1ea1a
ronak raj <126279165+ronakraj00@users.noreply.github.com>
2024-01-18 00:59:10 +0530

    add transtion in popup box

--- script.js
+++ script.js
@@ -4,61 +4,74 @@ const searchButton = document.querySelector("form button");
 const errorMessage = document.querySelector("#error");
 
 searchButton.addEventListener("click", () => {
-  fetchImg(query.value);
+    fetchImg(query.value);
 });
 
 async function fetchImg(query) {
-  errorMessage.textContent = "";
+    errorMessage.textContent = "";
 
-  try {
-    const fetchedData = await fetch(
-      `https://api.giphy.com/v1/gifs/translate?api_key=zEN1LyMwQsOt02o2sG1i2cXquWWAVE1R&s=${query}`,
-      { mode: "cors" }
-    );
+    try {
+        const fetchedData = await fetch(
+            `https://api.giphy.com/v1/gifs/translate?api_key=zEN1LyMwQsOt02o2sG1i2cXquWWAVE1R&s=${query}`,
+            { mode: "cors" }
+        );
 
-    const imgData = await fetchedData.json();
+        const imgData = await fetchedData.json();
 
-    createImage(imgData.data.images.original.url);
-  } catch (error) {
-    errorMessage.textContent = error.message;
-  }
+        createImage(imgData.data.images.original.url);
+    } catch (error) {
+        errorMessage.textContent = error.message;
+    }
 }
 
 function createImage(src) {
-  const img = document.createElement("img");
-  img.src = src;
-  img.onclick = () => confirm(src);
-  imgDiv.insertBefore(img, imgDiv.firstChild);
+    const img = document.createElement("img");
+    img.src = src;
+    img.onclick = () => confirm(src);
+    imgDiv.insertBefore(img, imgDiv.firstChild);
 }
 
-function confirm(src){
-  const popUp=document.querySelector(".confirm-popup");
-  popUp.classList.remove("hide-popup");
-  const cancelBtn=document.querySelector(".cancelbtn")
-  const downloadBtn=document.querySelector(".downloadbtn")
+function confirm(src) {
+    const popUp = document.querySelector(".confirm-popup");
+    const popUpDiv = document.querySelector(".confirm-popup > div");
+    popUp.classList.remove("hide-popup");
+    const cancelBtn = document.querySelector(".cancelbtn");
+    const downloadBtn = document.querySelector(".downloadbtn");
 
-  popUp.addEventListener("click",(e)=>{
-    e.stopPropagation();
-    popUp.classList.add("hide-popup");
-  })
+    setTimeout(() => {
+      popUpDiv.classList.add("fade-in");
+  }, 100);
+    popUp.addEventListener("click",hidePop);
+    downloadBtn.disabled=false;
+    cancelBtn.addEventListener("click",hidePop);
+    downloadBtn.addEventListener(
+        "click",
+        () => {
+          downloadBtn.disabled=true;
+            downloadImage(src);
+            hidePop();
+        },
+        { once: true }
+    );
+}
 
-  cancelBtn.addEventListener("click",()=>{
+function hidePop() {
+  setTimeout(() => {
+    const popUp = document.querySelector(".confirm-popup");
     popUp.classList.add("hide-popup");
-  })
-  downloadBtn.addEventListener("click",()=>{
-    downloadImage(src);
-    popUp.classList.add("hide-popup");
-  },{once:true});
+    const popUpDiv = document.querySelector(".popup");
+    popUpDiv.classList.remove("fade-in");
+}, 100);
 }
 
 async function downloadImage(imageSrc) {
-  const image = await fetch(imageSrc);
-  const imageBlog = await image.blob();
-  const imageURL = URL.createObjectURL(imageBlog);
-  const link = document.createElement("a");
-  link.href = imageURL;
-  link.download = "";
-  document.body.appendChild(link);
-  link.click();
-  document.body.removeChild(link);
+    const image = await fetch(imageSrc);
+    const imageBlog = await image.blob();
+    const imageURL = URL.createObjectURL(imageBlog);
+    const link = document.createElement("a");
+    link.href = imageURL;
+    link.download = "";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
 }
